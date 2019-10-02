75482273436ace4a7aa01d75676aefd89e831178
Matt Groberg <grobergmg@gmail.com>
2019-10-01 17:04:12 -0700

    add realistic clouds

--- scripts/index.js
+++ scripts/index.js
@@ -71,11 +71,60 @@ class CircleClipper extends MouseTracker{
 	}
 }
 
-const sections= ['design','js','blog','data']
+function rn(from, to) {
+  return ~~(Math.random() * (to - from + 1)) + from;
+}
+
+function rs() {
+  return arguments[rn(1, arguments.length) - 1];
+}
+
+function boxShadows(max) {
+  let ret = [];
+  for (let i = 0; i < max; ++i) {
+    ret.push(`
+      ${ rn(1, 100) }vw ${ rn(1, 100) }vh ${ rn(20, 40) }vmin ${ rn(1, 20) }vmin
+      ${ rs('rgb(200,200,200)', 'rgb(255,255,255)', 'rgb(230,230,230)', 'rgb(220,220,220)') }
+    `)
+  }
+  return ret.join(',');
+}
+
+function makeClouds(cloud) {
+  cloud.style.boxShadow = boxShadows(100);
+}
+
+function refreshClouds(cloud,fps){
+	cloudMove(cloud,fps)
+	setInterval(()=>{
+		cloudMove(cloud,fps)
+	},230*fps)	
+}
+
+function cloudMove(cloud,fps){
+	makeClouds(cloud)
+	let position=-100
+	for (let i=0;i<=230;i++){
+		setTimeout(()=>{
+			position++
+			cloud.style.left=`${position}vw`
+		},fps*i)
+	}
+}
+
 document.addEventListener("DOMContentLoaded", function(){
+	const sections= ['design','js','blog','data']
 	sections.forEach(section=>{
 		const newCircle=new CircleButton(60,section)
 		newCircle.buttonListeners()
 	})
 
+const topCloud = document.querySelector('#top-cloud');
+const mist1 = document.querySelector('#mist1');
+const mist2 = document.querySelector('#mist2');
+const mist3 = document.querySelector('#mist3');
+refreshClouds(topCloud,60)
+refreshClouds(mist1,90)
+refreshClouds(mist2,35)
+refreshClouds(mist3,15)
 });
